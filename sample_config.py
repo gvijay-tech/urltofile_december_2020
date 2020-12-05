7fea9c5f910f72af0ae9c38e793c05cd36c812ce
gvijay-tech <60471164+gvijay-tech@users.noreply.github.com>
2020-12-05 21:23:23 +0530

    changed thumbnail link
    added telegraph thumbnail link

--- sample_config.py
+++ sample_config.py
@@ -24,7 +24,7 @@ class Config(object):
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "http://extraimage.com/images/2020/05/02/IMG_20200407_132741_200c1acc3a77ebce334.jpg")
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://telegra.ph/file/f8bc3d3d2877b6fe65eb2.jpg")
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
